bd8d06ffa5909a7f4bdd16897e93264a80b1774f
curtiszimmerman <software@curtisz.com>
2014-06-22 00:06:55 -0700

    added: @plans

--- visinot.py
+++ visinot.py
@@ -11,12 +11,35 @@
 # @license GPLv3
 # @version 0.0.1a
 
+##
+# @plan take picture every half second, compare to LAST image taken, and compare to image from
+# one minute ago (or ten minutes or one hour). notify via curl on difference. the reference 
+# image from one/ten/sixty minutes prior is to prevent very slow-moving people from taking 
+# advantage of error margin (like is possible with most motion detection, to move very slowly
+# and fool the sensor since its reference image doesn't change enough to trigger it)
+
 __version__ = "0.0.1a"
+
+def notify( image ):
+	return 0
 
 def main():
 	from SimpleCV import Camera, Color, Display, Image
-	import random
 	import time
+
+	# initialize camera
+
+	# while forever:
+	#	img = cam.getImage()
+	#	timestamp
+	#	if (no reference.timestamp):
+	#		reference.image = this.image
+	#	if (no last.image):
+	# 
+	#	if (this.timestamp - ten minutes) is greater than reference.timestamp:
+	#		reference.image = this.image
+	#		last.image = this.image
+	#	time.sleep(0.4)
 
 	# init camera with forced dimensions
 	cam = Camera(0, {"width": 640, "height": 480})
